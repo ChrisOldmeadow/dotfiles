e808ce990db04e0aaf2252f2114de782ef1e73d5
Chris Oldmeadow <chris.oldmeadow@mailbox.org>
2021-06-19 15:51:55 +1000

    legion

--- .config/nvim/lua/colourscheme.lua
+++
@@ -1,3 +0,0 @@
-vim.cmd('colorscheme palenight')
-vim.cmd('let g:nvcode_termcolors=256')
-
--- .config/nvim/lua/nvim-vimwiki/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.vimwiki_list = { {path = '~/Nextcloud/Notes/General', syntax = 'markdown', ext = '.md'},
-                       {path = '~/Nextcloud/Notes/Computing', syntax ='markdown', ext = '.md'},
-                       {path = '~/Nextcloud/Notes/R', syntax = 'markdown', ext = '.md'},
-                       {path = '~/Nextcloud/Notes/Goals', syntax = 'markdown', ext = '.md'},
-                       {path = '~/Nextcloud/Notes/Management', syntax = 'markdown', ext = '.md'},
-                       {path = '~/Nextcloud/Notes/Haskell', syntax = 'markdown', ext = '.md'} }
-
-
